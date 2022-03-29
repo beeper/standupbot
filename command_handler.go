75d45d5b508559918f53a398e5f0763aa80481f4
Sumner Evans <me@sumnerevans.com>
2022-03-29 10:17:42 -0600

    threads: allow stable m.thread

--- command_handler.go
+++ command_handler.go
@@ -522,7 +522,7 @@ func HandleMessage(_ mautrix.EventSource, event *mevent.Event) {
 			if relatesTo != nil {
 				if relatesTo.Type == mevent.RelReplace {
 					HandleEdit(event, val)
-				} else if relatesTo.Type == mevent.RelReply || relatesTo.Type == "io.element.thread" {
+				} else if relatesTo.Type == mevent.RelReply || relatesTo.Type == "io.element.thread" || relatesTo.Type == "m.thread" {
 					HandleReply(event, val)
 				}
 				return
